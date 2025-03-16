a8eaa0934ea35d73775881dc67d327225c87cb46
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2025-03-16 00:15:56 -0300

    only 1 commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log("Llego algo");
+  
   
   app.enableCors({
     origin: ['http://localhost:5173', 'http://noxus.com.ar'],
@@ -11,6 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
