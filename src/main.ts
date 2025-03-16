977aa3938181d50a4b77e253ee9bf38f904a862b
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2025-03-15 21:42:42 -0300

    replace https for http in order to try

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://noxus.com.ar'],
+    origin: ['http://localhost:5173', 'http://noxus.com.ar'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
