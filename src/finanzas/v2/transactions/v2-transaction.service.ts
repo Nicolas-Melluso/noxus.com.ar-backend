12790d82468dca9bc3c89be5502927013f7e9b6b
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2026-01-26 16:31:13 -0300

    aa

--- src/finanzas/v2/transactions/v2-transaction.service.ts
+++ src/finanzas/v2/transactions/v2-transaction.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { V2BalanceService } from '../balance/v2-balance.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { V2Transaction } from './v2-transaction.entity';
@@ -8,6 +9,7 @@ export class V2TransactionService {
   constructor(
     @InjectRepository(V2Transaction)
     private readonly v2TransactionRepo: Repository<V2Transaction>,
+    private readonly v2BalanceService: V2BalanceService,
   ) {}
 
   async getUserTransactions(userId: number): Promise<V2Transaction[]> {
@@ -20,6 +22,22 @@ export class V2TransactionService {
     // Insertar nuevas
     const txs = transactions.map(t => ({ ...t, userId, deleted: false }));
     await this.v2TransactionRepo.save(txs);
+
+    // Recalcular balance
+    // Sumar ingresos, egresos y deudas por moneda
+    const balanceData = {
+      usd_ingresos: 0, usd_egresos: 0, usd_deudas: 0,
+      ars_ingresos: 0, ars_egresos: 0, ars_deudas: 0,
+      eur_ingresos: 0, eur_egresos: 0, eur_deudas: 0
+    };
+    for (const tx of txs) {
+      const { amount = 0, type, currency = 'ARS' } = tx;
+      if (type === 'income') balanceData[`${currency.toLowerCase()}_ingresos`] += Number(amount);
+      else if (type === 'expense') balanceData[`${currency.toLowerCase()}_egresos`] += Number(amount);
+      else if (type === 'debt') balanceData[`${currency.toLowerCase()}_deudas`] += Number(amount);
+    }
+    await this.v2BalanceService.setUserBalance(userId, balanceData);
+
     return { saved: txs.length };
   }
 }
